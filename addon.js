8bcd0f9a6f9fda69d17b01ce9e975d10838cdfc6
Lukáš <kyselalukas@seznam.cz>
2026-09-24 08:40:13 +0200

    Read runtime version from package.json

--- addon.js
+++ addon.js
@@ -21,7 +21,7 @@ const {
 
 const app = express();
 const PORT = process.env.PORT || 7000;
-const VERSION = '0.5.1';
+const VERSION = require('./package.json').version;
 const DEBUG_STREAMUJ_RAW = process.env.DEBUG_STREAMUJ_RAW === '1';
 const cache = new NodeCache({ stdTTL: 300, checkperiod: 60 });
 const idMapCache = new NodeCache({ stdTTL: 24 * 60 * 60, checkperiod: 10 * 60 });
